e6c5c67dea97e75a6696d0e9ff6a234729b07fdb
Joseph Stephens <josephstephensbusiness@gmail.com>
2024-09-13 05:10:05 -0400

    build

---
+++ BUILD/include/mio/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/~DEV/--PROJECTS/SimpleGLRenderer/SimpleGLRenderer/vendor/mio/include/mio
+# Build directory: D:/~DEV/--PROJECTS/SimpleGLRenderer/SimpleGLRenderer/vendor/mio/BUILD/include/mio
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("detail")
---
+++ BUILD/include/mio/detail/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/~DEV/--PROJECTS/SimpleGLRenderer/SimpleGLRenderer/vendor/mio/include/mio/detail
+# Build directory: D:/~DEV/--PROJECTS/SimpleGLRenderer/SimpleGLRenderer/vendor/mio/BUILD/include/mio/detail
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
